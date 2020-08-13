487d97bb28209fccb91fc8498f5b55f0dfd08169
MarkHu0423 <1512571700@qq.com>
2020-08-13 20:27:03 +0800

    第二次修改（日线）

--- commercial.py
+++ commercial.py
@@ -11,9 +11,9 @@ print('login respond  error_msg:'+lg.error_msg)
 #### 获取沪深A股历史K线数据 ####
 
 rs = bs.query_history_k_data_plus("sh.600000",
-    "date,time,code,open,high,low,close,volume,amount",
-    start_date='2019-12-01', end_date='2019-12-05',
-    frequency="15", adjustflag="3")
+    "date,code,open,high,low,close,preclose,volume,amount",
+    start_date='2019-01-01', end_date='2019-12-31',
+    frequency="d", adjustflag="3")
     # code：股票代码，sh或sz.+6位数字代码，或者指数代码，如：sh.601398。sh：上海；sz：深圳。此参数不可为空；
     # fields：指示简称，支持多指标输入，以半角逗号分隔，填写内容作为返回类型的列。详细指标列表见历史行情指标参数章节，日线与分钟线参数不同。此参数不可为空；
     # start：开始日期（包含），格式“YYYY-MM-DD”，为空时取2015-01-01；
@@ -37,15 +37,18 @@ print(result)
 #### 登出系统 ####
 bs.logout()
 
+# 导入数据分析库pandas，官方文档https://pandas.pydata.org/docs/
 import pandas as pd
 
+# 从本地导入数据，这里用的是相对路径，如果你的程序和文件不在同一个文件夹里请用绝对路径
 df = pd.read_csv('_A_stock_k_data.csv')
 # 查看数据
 df.head()
+# 剔除缺失数据
 df = df.dropna()
-
-raw_time = pd.to_datetime(df.pop('time'), format='%Y/%m/%d %H:%M')
-
+df.head()
+# 取出时间
+raw_time = pd.to_datetime(df.pop('date'), format='%Y/%m/%d')
 from matplotlib import pyplot as plt
 import seaborn as sns
 
