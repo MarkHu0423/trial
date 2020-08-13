034cbd40fddb6d23def0732407cb139876ba0243
djx000 <18971057460@163.com>
2020-08-14 01:03:28 +0800

    董佳鑫 函数：转换日期数据类型

---
+++ transform.py
@@ -0,0 +1,7 @@
+def date_to_num(dates):
+    num_time = []
+    for date in dates:
+        date_time = datetime.datetime.strptime(date,'%Y-%m-%d')
+        num_date = date2num(date_time)
+        num_time.append(num_date)
+    return num_time
